fdb3bead9c4a606ad869b0c321f1cc28b33dd8b9
mamebassine <mamebassine06@gmail.com>
2025-08-25 16:47:14 +0000

    Changement sur les window

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Tymon\JWTAuth\Contracts\JWTSubject;  
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject  
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
