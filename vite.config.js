5e3aa10d64cbfdb1c5e9c89c013e400fc092cf0a
mamebassine <mamebassine06@gmail.com>
2025-09-17 12:45:35 +0000

    Terminer les parrainnages

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,31 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    server: {
+        proxy: {
+            // toutes les requêtes commençant par /api seront redirigées vers Laravel
+            '/api': 'http://localhost:8000',
+        }
+    }
 });
+
+
+
+
+
+
+
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//         tailwindcss(),
+//     ],
+// });
