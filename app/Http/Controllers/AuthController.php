fbb69ecc1388bb346b429eb5d755e33bf35e2d7b
mamebassine <mamebassine06@gmail.com>
2025-09-24 01:24:51 +0000

    Fatiguer email

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -123,11 +123,20 @@ public function register(Request $request)
         }
 
 // --- AJOUTER ICI POUR ENVOYER LE MAIL ---
-$lienParrainage = "http://localhost:5173/userlogin";
+
+// .env (local)
+// $lienParrainage = "http://localhost:5173/userlogin";
+
+// .env (production)
+//$lienParrainage = "http://vendeur-pro.com/userlogin";
+
+// Générer automatiquement le lien de parrainage selon l'environnement
+$lienParrainage = env('APP_URL') . '/userlogin';
+
+// Envoyer la notification avec le lien
 $user->notify(new ParrainCreatedTest($user, $lienParrainage, $request->password));
 
-
-       DB::commit();
+DB::commit();
 
 // Retour JSON en utilisant uniquement le lien stocké
 return response()->json([
